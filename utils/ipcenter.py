d60b1b4c3aafd7f9b5e4fb4d12cb8803d393ac66
Yang Dx <inboxcvt@gmail.com>
2017-10-12 11:28:58 +0800

    防止sql注入，使用pymysql的格式化

--- utils/ipcenter.py
+++ utils/ipcenter.py
@@ -31,10 +31,10 @@ DBdrive = DB()
 def exec_query_ip(ip='127.0.0.1'):
     ret = {'status': True,
            'data': ''}
-    query_sql = "SELECT * FROM `aiwen_free_district_v2.0.1` WHERE minip <= INET_ATON('{_query_ip}') ORDER BY minip DESC LIMIT 1;".format(_query_ip=ip)
+    query_sql = "SELECT * FROM `aiwen_free_district_v2.0.1` WHERE minip <= INET_ATON(%s) ORDER BY minip DESC LIMIT 1"
     try:
         with DBdrive.re_conn().cursor() as cursor:
-            cursor.execute(query_sql)
+            cursor.execute(query_sql, (ip,))
             result = cursor.fetchone()
             ret['data'] = result
     except Exception as e:
@@ -47,20 +47,22 @@ def exec_count_ip(ip='127.0.0.1'):
     ret = {'status': True,
            'data': ''}
 
-    query_sql = "SELECT * FROM `user_ip_count` WHERE ip = '{_c_ip}';".format(_c_ip=ip)
-    update_sql = "UPDATE `user_ip_count` SET ip_count=ip_count+1 where ip = '{_c_ip}';".format(_c_ip=ip)
-    insert_sql = "INSERT INTO user_ip_count(ip,ip_count) VALUES (%s,%s);"
+    query_sql = "SELECT * FROM `user_ip_count` WHERE ip = %s"
+    update_sql = "UPDATE `user_ip_count` SET ip_count=ip_count+1 where ip = %s"
+    insert_sql = "INSERT INTO user_ip_count(ip,ip_count) VALUES (%s,%s)"
     try:
         with DBdrive.re_conn().cursor() as cursor:
-            cursor.execute(query_sql)
+            cursor.execute(query_sql, (ip,))
             result = cursor.fetchone()
-            print(result)
+            #print(result)
             if result:
-                cursor.execute(update_sql)
+                cursor.execute(update_sql, (ip,))
+                cursor.execute(query_sql, (ip,))
+                result = cursor.fetchone()
                 DBdrive.re_conn().commit()
             else:
                 cursor.execute(insert_sql, (ip, 1))
-                cursor.execute(query_sql)
+                cursor.execute(query_sql, (ip,))
                 result = cursor.fetchone()
                 DBdrive.re_conn().commit()
             ret['data'] = result
